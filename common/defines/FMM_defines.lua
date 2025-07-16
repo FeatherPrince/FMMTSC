e228fd94a2f89551b89ed047a9f157978a82ec78
Feather <featherprinceyt@gmail.com>
2025-07-16 02:47:27 +0200

    commit

--- common/defines/FMM_defines.lua
+++ common/defines/FMM_defines.lua
@@ -1,1 +1,25 @@
 NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.2, 0.1, 0.05, 0.0 }; --default 	GAME_SPEED_SECONDS = { 2.0, 0.5, 0.2, 0.1, 0.0 }, -- game speeds for each level. Must be 5 entries with last one 0 for unbound
+NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 6; -- default 2
+
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 999;			--Max army experience a country can store
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 999;			--Max navy experience a country can store
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 999;				--Max air experience a country can store
+
+NDefines.NNavy.NAVAL_INVASION_PRIORITY = 1;									-- Default convoy priority for naval invasions
+NDefines.NNavy.NAVAL_TRANSFER_PRIORITY = 1;									-- Default convoy priority for naval transports
+NDefines.NNavy.SUPPLY_PRIORITY = 2;											-- Default convoy priority for supplying units via sea
+NDefines.NNavy.RESOURCE_LENDLEASE_PRIORITY = 10;		--default 3						-- Default convoy priority for export lend lease
+NDefines.NNavy.RESOURCE_EXPORT_PRIORITY = 4;									-- Default convoy priority for export trade
+NDefines.NNavy.RESOURCE_ORIGIN_PRIORITY = 5;									-- Default convoy priority for resources shipped internally
+NDefines.NNavy.RESOURCE_PURCHASE_PRIORITY = 6;									-- Default convoy priority for export equipment purchase
+NDefines.NNavy.UNDERWAY_REPLENISHMENT_PRIORITY = 7;							-- Default convoy priority for underway replenishment
+
+
+NDefines.NOperatives.MAX_OPERATIVE_SLOT_FROM_AGENCY_UPGRADES = 3;	--default 1 -- max operative slots gained from upgrades
+
+
+--NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 5;			-- Max width of regiments in division designer.
+--NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 5;			-- Max height of regiments in division designer.
+--NDefines.NMilitary.MIN_DIVISION_BRIGADE_HEIGHT = 4;			-- Min height of regiments in division designer.
+--NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2;			-- Max width of support in division designer.
+--NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 5;			-- Max height of support in division designer.
